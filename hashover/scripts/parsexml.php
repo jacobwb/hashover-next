cb26346868a3e63c1dc4beb59962f27ba04b9214
Jacob Barkdull <jacobbarkdull@gmail.com>
2015-04-20 21:52:18 -0700

    Fixed: HTML not being escaped

--- hashover/scripts/parsexml.php
+++ hashover/scripts/parsexml.php
@@ -1,6 +1,6 @@
 <?php
 
-	// Copyright (C) 2014 Jacob Barkdull
+	// Copyright (C) 2015 Jacob Barkdull
 	//
 	//	This program is free software: you can redistribute it and/or modify
 	//	it under the terms of the GNU Affero General Public License as
@@ -53,30 +53,40 @@
 
 		public function save(array $contents, $file, $editing = false, $fullpath = false)
 		{
+			if ($fullpath == false) {
+				$file = $this->setup->dir . '/' . $file . '.xml';
+			}
+
+			if (file_exists($file) and $editing == false) {
+				return false;
+			}
+
 			// Create empty XML DOM document
 			$dom = new DOMDocument('1.0', 'UTF-8');
 			$dom->preserveWhiteSpace = false;
 			$dom->formatOutput = true;
 
-			// Create and append the root element "comment"
+			// Create root element "comment"
 			$comment = $dom->createElement('comment');
+
+			// Add comment data to root "comment" element
+			foreach ($contents as $key => $value) {
+				$element = $dom->createElement($key);
+				$text_node = $dom->createTextNode($value);
+				$element->appendChild($text_node);
+				$comment->appendChild($element);
+			}
+
+			// Append root element "comment"
 			$dom->appendChild($comment);
 
-			foreach ($contents as $key => $value) {
-				$comment->appendChild($dom->createElement($key, $value));
-			}
+			// Replace double spaces with single tab
+			$tabbed_dom = str_replace('  ', "\t", $dom->saveXML());
 
-			if ($fullpath == false) {
-				$file = $this->setup->dir . '/' . $file . '.xml';
-			}
-
-			if (!file_exists($file) or $editing == true) {
-				$tabbed_dom = str_replace('  ', "\t", $dom->saveXML());
-
-				if (file_put_contents($file, $tabbed_dom, LOCK_EX)) {
-					chmod($file, 0600);
-					return true;
-				}
+			// Attempt to write file
+			if (file_put_contents($file, $tabbed_dom, LOCK_EX)) {
+				chmod($file, 0600);
+				return true;
 			}
 
 			return false;
