2f1315f015d8fce07650275f5a692c4cd7374538
ale rimoldi <ale.comp_06@xox.ch>
2015-10-25 08:07:02 +0100

    allow custom rootDirectory and httpDirectory
    by default, initialize rootDirectory and httpDirectory to null and check for their definition before setting them to the automatic value

--- hashover/scripts/settings.php
+++ hashover/scripts/settings.php
@@ -110,8 +110,8 @@
 		public $isMobile		= false;
 
 		// Technical settings placeholders
-		public $rootDirectory;
-		public $httpDirectory;
+		public $rootDirectory		= null;
+		public $httpDirectoryx		= null;
 		public $cookieExpiration;
 		public $domain;
 
@@ -128,8 +128,12 @@
 			$dirname = dirname (__DIR__);
 
 			// Technical settings
-			$this->rootDirectory	= $dirname;			// Root directory for script
-			$this->httpDirectory	= '/' . basename ($dirname);	// Root directory for HTTP
+			if (!isset($this->rootDirectory)) {
+				$this->rootDirectory	= $parent_directory;		// Root directory for script
+			}
+			if (!isset($this->httpDirectory)) {
+				$this->httpDirectory	= '/' . basename ($parent_directory);	// Root directory for HTTP
+			}
 			$this->cookieExpiration	= time () + 60 * 60 * 24 * 30;	// Cookie expiration date
 			$this->domain		= $_SERVER['HTTP_HOST'];	// Domain name for refer checking & notifications
 
