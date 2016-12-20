a619088aeff0670623dd518373b93d5fb1637a99
Jacob Moen <jacmoe.dk@gmail.com>
2016-12-20 17:19:01 +0100

    Nitpick in description for `setCookies`

--- hashover/scripts/settings.php
+++ hashover/scripts/settings.php
@@ -78,7 +78,7 @@ class Settings
 	// Technical settings
 	public $loginMethod		= 'defaultLogin';		// Login method class for handling user login information
 	public $secureCookies		= false;			// Whether cookies set over secure HTTPS will only be transmitted over HTTPS
-	public $setCookies			= true;				// Whether cookies set at all
+	public $setCookies			= true;				// Whether cookies are set at all
 	public $storesIPAddress		= false;			// Whether to store users' IP addresses
 	public $allowsUserReplies	= false;			// Whether given e-mails are sent as reply-to address to users
 	public $noreplyEmail		= 'noreply@example.com';	// E-mail used when no e-mail is given
