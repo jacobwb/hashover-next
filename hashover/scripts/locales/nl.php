b7678185f8172c08086111c22df7951ee5c06b56
jacobwb <jacobbarkdull@gmail.com>
2017-01-05 04:34:15 -0800

    Update Copyright years

--- hashover/scripts/locales/nl.php
+++ hashover/scripts/locales/nl.php
@@ -1,6 +1,6 @@
 <?php
 
-// Copyright (C) 2016 Jacob Barkdull
+// Copyright (C) 2016-2017 Jacob Barkdull
 // This file is part of HashOver.
 //
 // I, Jacob Barkdull, hereby release this work into the public domain.
