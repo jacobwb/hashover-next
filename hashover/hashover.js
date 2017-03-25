4851af026449256a9f40f83b466b033493bcbee3
jacobwb <jacobbarkdull@gmail.com>
2017-03-25 09:35:57 -0700

    Improved how HashOver accesses its loader script.
    Removed `hashover-wp-plugin.php`, please see the repository by the same name for that code.

--- hashover/hashover.js
+++ hashover/hashover.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2010-2015 Jacob Barkdull
+// Copyright (C) 2010-2017 Jacob Barkdull
 // This file is part of HashOver.
 //
 // HashOver is free software: you can redistribute it and/or modify
@@ -45,15 +45,34 @@
 (function () {
 	"use strict";
 
+	var loaderScript = document.getElementById ('hashover-loader');
+	var thisScript = document.currentScript || loaderScript;
 	var scripts = document.getElementsByTagName ('script');
-	var scriptNumber = scripts.length;
-	var thisScript = scripts[scriptNumber - 1];
+	var scriptNumber = 0;
+
 	var location = window.location.href.split ('#');
 	var scriptQueries = 'url=' + encodeURIComponent (location[0]);
-	var thisQueries = thisScript.src.split ('?')[1];
 	var scriptPath = '/scripts/hashover-javascript.php';
 	var api = false;
-	var script;
+	var div, script;
+
+	// Check if we get have the script element
+	if (thisScript) {
+		// If so, find the script's index on the page
+		for (var i = 0, il = scripts.length; i < il; i++) {
+			if (scripts[i] === thisScript) {
+				scriptNumber = i;
+				break;
+			}
+		}
+	} else {
+		// If not, use the last script encountered
+		scriptNumber = scripts.length;
+		thisScript = scripts[scriptNumber - 1];
+	}
+
+	// Get URL queries
+	var thisQueries = thisScript.src.split ('?')[1];
 
 	// Simple JavaScript port of PHP's dirname
 	function dirname (string)
@@ -115,22 +134,24 @@
 
 	// HTML div tag for HashOver comments to appear in
 	if (document.getElementById ('hashover') === null) {
-		var div = document.createElement ('div');
-		    div.id = 'hashover';
+		div = document.createElement ('div');
+		div.id = 'hashover';
 
 		// Insert HTML div tag before current script tag
 		thisScript.parentNode.insertBefore (div, thisScript);
 	}
 
 	// Append HashOver JavaScript tag to the page body
-	script = document.createElement ('script');
-	script.type = 'text/javascript';
-	script.src  = dirname (thisScript.getAttribute ('src')) + scriptPath;
-	script.src += '?' + scriptQueries;
-	script.src += '&' + 'hashover-script=' + scriptNumber;
-	script.id = 'hashover-script-' + scriptNumber;
-	script.async = true;
+	if (thisScript.getAttribute ('src')) {
+		script = document.createElement ('script');
+		script.type = 'text/javascript';
+		script.src  = dirname (thisScript.getAttribute ('src')) + scriptPath;
+		script.src += '?' + scriptQueries;
+		script.src += '&' + 'hashover-script=' + scriptNumber;
+		script.id = 'hashover-script-' + scriptNumber;
+		script.async = true;
 
-	// Append to parent element of current script tag
-	thisScript.parentNode.appendChild (script);
+		// Append to parent element of current script tag
+		thisScript.parentNode.appendChild (script);
+	}
 }) ();
