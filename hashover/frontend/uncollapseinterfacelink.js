70b5e7c3db8c37c207be1d9820c1d8aeebe53621
jacobwb <jacobbarkdull@gmail.com>
2018-09-02 02:02:49 -0700

    Refactoring for upcoming commit

--- hashover/frontend/uncollapseinterfacelink.js
+++ hashover/frontend/uncollapseinterfacelink.js
@@ -1,26 +1,30 @@
 // Creates the interface uncollapse button (uncollapseinterfacelink.js)
 HashOver.prototype.uncollapseInterfaceLink = function ()
 {
-	// Decide button text
-	var show = 'show-number-comments';
-	var post = 'post-comment-on';
-	var uncollapseLocale = (this.instance['total-count'] >= 1) ? show : post;
-	var uncollapseText = this.instance[uncollapseLocale];
+	// Main element
+	var main = this.instance['main-element'];
 
-	// Create hyperlink to uncollapse the comment interface
-	var uncollapseLink = this.elements.create ('a', {
+	// Check if there is one or more comments
+	if (this.instance['total-count'] >= 1) {
+		// If so, use "Show X More Comments" locale
+		var text = this.instance['show-number-comments'];
+	} else {
+		// If not, show "Post Comment on ..." locale
+		var text = this.instance['post-comment-on'];
+	}
+
+	// Create hyperlink to uncollapse the interface
+	main.appendChild (this.elements.create ('a', {
 		id: 'hashover-uncollapse-interface-link',
 		className: 'hashover-more-link',
 		href: '#',
-		title: uncollapseText,
-		textContent: uncollapseText,
+		title: text,
+		text: text,
 
+		// Uncollapse the interface when clicked
 		onclick: function () {
 			hashover.uncollapseInterface ();
 			return false;
 		}
-	});
-
-	// Add uncollapse hyperlink to HashOver div
-	hashover.instance['main-element'].appendChild (uncollapseLink);
+	}));
 };
