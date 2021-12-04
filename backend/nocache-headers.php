9d01aa708cdb559cf80fbe45b238115474f14f97
Daniel Aleksandersen <code@daniel.priv.no>
2021-12-04 04:30:10 +0100

    Enable HTTP caching for Count and Latest APIs
    Static JavaScript assets (unless the admin changes settings)
    get 0:30–24:00 hour freshness with async revalidating.
    
    The related API responses get 2–58 second freshness, with caches
    retaining stale data for up to 5 min on server errors.

--- backend/nocache-headers.php
+++ backend/nocache-headers.php
@@ -18,8 +18,5 @@
 
 
 // Disable browser cache
-header ('Expires: Wed, 08 May 1991 12:00:00 GMT');
-header ('Last-Modified: ' . gmdate ('D, d M Y H:i:s') . ' GMT');
-header ('Cache-Control: no-store, no-cache, must-revalidate');
-header ('Cache-Control: post-check=0, pre-check=0', false);
+header ('Cache-Control: no-cache, max-age=0');
 header ('Pragma: no-cache');
